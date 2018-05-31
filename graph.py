d1177ebc0a690f46ae27625c26ef521375dbede5
Masaki Aota <masaki.aota@ruri.waseda.jp>
2018-05-31 15:54:00 +0900

    範囲の変更

--- graph.py
+++ graph.py
@@ -8,22 +8,24 @@ import matplotlib.pyplot as plt
 PORT = '/dev/tty.MindWaveMobile-SerialPo'
 
 # matplotlibのデフォルト設定の変更
-font = {'family':'monospace', 'size':'9'}
+font = {'family': 'monospace', 'size': '9'}
 mpl.rc('font', **font)
+
 
 class realtime_plot(object):
 
     def __init__(self):
-        self.fig = plt.figure(figsize=(12,8))
+        self.fig = plt.figure(figsize=(12, 8))
         self.initialize()
 
     def initialize(self):
         self.fig.suptitle('monitoring sample', size=12)
-        self.fig.subplots_adjust(left=0.05, bottom=0.1, right=0.95, top=0.90, wspace=0.2, hspace=0.6)
-        self.ax00 = plt.subplot2grid((2,2),(0,0))
-        self.ax10 = plt.subplot2grid((2,2),(1,0))
-        self.ax01 = plt.subplot2grid((2,2),(0,1))
-        self.ax11 = plt.subplot2grid((2,2),(1,1))
+        self.fig.subplots_adjust(
+            left=0.05, bottom=0.1, right=0.95, top=0.90, wspace=0.2, hspace=0.6)
+        self.ax00 = plt.subplot2grid((2, 2), (0, 0))
+        self.ax10 = plt.subplot2grid((2, 2), (1, 0))
+        self.ax01 = plt.subplot2grid((2, 2), (0, 1))
+        self.ax11 = plt.subplot2grid((2, 2), (1, 1))
         self.ax00.grid(True)
         self.ax10.grid(True)
         self.ax01.grid(True)
@@ -46,44 +48,44 @@ class realtime_plot(object):
 #        self.ax11.set_ylabel('predict')
 
         # プロットの初期化
-        self.lines000, = self.ax00.plot([-1,-1],[1,1],label='attention')
-        self.lines001, = self.ax00.plot([-1,-1],[1,1],label='meditation')
-        self.lines100, = self.ax10.plot([-1,-1],[1,1],label='delta')
-        self.lines101, = self.ax10.plot([-1,-1],[1,1],label='theta')
-        self.lines102, = self.ax10.plot([-1,-1],[1,1],label='lowalpha')
-        self.lines103, = self.ax10.plot([-1,-1],[1,1],label='highalpha')
-        self.lines104, = self.ax10.plot([-1,-1],[1,1],label='lowbeta')
-        self.lines105, = self.ax10.plot([-1,-1],[1,1],label='highbeta')
-        self.lines106, = self.ax10.plot([-1,-1],[1,1],label='lowgamma')
-        self.lines107, = self.ax10.plot([-1,-1],[1,1],label='midgamma')
-        self.lines01, = self.ax01.plot([-1,-1],[1,1],label='highalpha')
-        self.lines11, = self.ax11.plot([-1,-1],[1,1],label='lowgamma')
+        self.lines000, = self.ax00.plot([-1, -1], [1, 1], label='attention')
+        self.lines001, = self.ax00.plot([-1, -1], [1, 1], label='meditation')
+        self.lines100, = self.ax10.plot([-1, -1], [1, 1], label='delta')
+        self.lines101, = self.ax10.plot([-1, -1], [1, 1], label='theta')
+        self.lines102, = self.ax10.plot([-1, -1], [1, 1], label='lowalpha')
+        self.lines103, = self.ax10.plot([-1, -1], [1, 1], label='highalpha')
+        self.lines104, = self.ax10.plot([-1, -1], [1, 1], label='lowbeta')
+        self.lines105, = self.ax10.plot([-1, -1], [1, 1], label='highbeta')
+        self.lines106, = self.ax10.plot([-1, -1], [1, 1], label='lowgamma')
+        self.lines107, = self.ax10.plot([-1, -1], [1, 1], label='midgamma')
+        self.lines01, = self.ax01.plot([-1, -1], [1, 1], label='highalpha')
+        self.lines11, = self.ax11.plot([-1, -1], [1, 1], label='lowgamma')
 #        self.lines01, = self.ax01.plot([-1,-1],[1,1],'.')
 #        self.lines11, = self.ax11.plot([-1,-1],[1,1],'.r')
 
     # 値名と値を代入した辞書タイプのdataから，必要なデータをsubplotの値に代入します
-    def set_data(self,data):
+    def set_data(self, data):
 
-        self.lines000.set_data(data['x'],data['y1'])
-        self.lines001.set_data(data['x'],data['y2'])
-        self.ax00.set_xlim((data['x'].min(),data['x'].max()))
-        self.ax00.set_ylim((-1,101)) 
-        self.lines100.set_data(data['x'],data['delta'])
-        self.lines101.set_data(data['x'],data['theta'])
-        self.lines102.set_data(data['x'],data['lowalpha'])
-        self.lines103.set_data(data['x'],data['highalpha'])
-        self.lines104.set_data(data['x'],data['lowbeta'])
-        self.lines105.set_data(data['x'],data['highbeta'])
-        self.lines106.set_data(data['x'],data['lowgamma'])
-        self.lines107.set_data(data['x'],data['midgamma'])
-        self.ax10.set_xlim((data['x'].min(),data['x'].max()))
-        self.ax10.set_ylim((-0.01,1))
-        self.lines01.set_data(data['x'],data['highalpha_'])
-        self.lines11.set_data(data['x'],data['lowgamma_'])
-        self.ax01.set_xlim((data['x'].min(),data['x'].max()))
-        self.ax01.set_ylim((-0.01,1677721))
-        self.ax11.set_xlim((data['x'].min(),data['x'].max()))
-        self.ax11.set_ylim((-0.01,1677721))
+        self.lines000.set_data(data['x'], data['y1'])
+        self.lines001.set_data(data['x'], data['y2'])
+        self.ax00.set_xlim((data['x'].min(), data['x'].max()))
+        self.ax00.set_ylim((-1, 101))
+        self.lines100.set_data(data['x'], data['delta'])
+        self.lines101.set_data(data['x'], data['theta'])
+        self.lines102.set_data(data['x'], data['lowalpha'])
+        self.lines103.set_data(data['x'], data['highalpha'])
+        self.lines104.set_data(data['x'], data['lowbeta'])
+        self.lines105.set_data(data['x'], data['highbeta'])
+        self.lines106.set_data(data['x'], data['lowgamma'])
+        self.lines107.set_data(data['x'], data['midgamma'])
+        self.ax10.set_xlim((data['x'].min(), data['x'].max()))
+        self.ax10.set_ylim((-0.01, 1))
+        self.lines01.set_data(data['x'], data['highalpha_'])
+        self.lines11.set_data(data['x'], data['lowgamma_'])
+        self.ax01.set_xlim((data['x'].min(), data['x'].max()))
+        self.ax01.set_ylim((-0.01, 167772))
+        self.ax11.set_xlim((data['x'].min(), data['x'].max()))
+        self.ax11.set_ylim((-0.01, 167772))
         # 凡例を固定するために必要
         self.ax00.legend(loc='upper right')
         self.ax10.legend(loc='upper left')
@@ -92,8 +94,9 @@ class realtime_plot(object):
 #        self.ax01.set_xlim((-2,12))
 #        self.ax01.set_ylim((-2,12))
 
-    def pause(self,second):
+    def pause(self, second):
         plt.pause(second)
+
 
 # 使用例
 RP = realtime_plot()
@@ -122,12 +125,12 @@ data['lowgamma_'] = np.zeros(100)
 #m = np.zeros(100)
 #m = np.zeros(100)
 opt_coef = 0
-attention = 5 
+attention = 0
 meditation = 0
-highalpha = 0 
+highalpha = 0
 lowgamma = 0
-masaki_ = [np.zeros(100)]*8
-masaki = [0]*8
+masaki_ = [np.zeros(100)] * 8
+masaki = [0] * 8
 
 for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
     for pkt in packets:
@@ -140,25 +143,26 @@ for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
             differencer = t[0:1]
             if int(differencer) == 1:
                 attention = t[1:]
-                print 'attention: %d' %int(attention)
+                print 'attention: %d' % int(attention)
             if int(differencer) == 2:
                 meditation = t[1:]
-                print 'meditation: %d' %int(meditation)
+                print 'meditation: %d' % int(meditation)
             if int(differencer) == 5:
                 eeg = t[1:]
-                #print eeg
+                # print eeg
                 l = eeg.split(", ")
                 l = [x.split("=") for x in l]
-                l[len(l)-1][1] = l[len(l)-1][1].replace(')','')
+                l[len(l) - 1][1] = l[len(l) - 1][1].replace(')', '')
                 #print([l[i][1] for i in range(len(l))])
                 masaki = [int(l[i][1]) for i in range(len(l))]
                 highalpha = masaki[3]
                 lowgamma = masaki[6]
-                masaki[2:] = [float(masaki[i])/sum(masaki[2:]) for i in range(2,len(masaki))]
-            
+                masaki[2:] = [float(masaki[i]) / sum(masaki[2:])
+                              for i in range(2, len(masaki))]
+
             tm.sleep(0.2)
             time += 1
-            x = np.arange(-5+0.1*time, 5+0.1*time, 0.1)
+            x = np.arange(-5 + 0.1 * time, 5 + 0.1 * time, 0.1)
 #            y1_new = np.append(y1[1:], np.array([int(attention)]))
 #            y1 = y1_new
 #            y2_new = np.append(y2[1:], np.array([int(meditation)]))
@@ -168,13 +172,21 @@ for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
             data['y2'] = np.append(data['y2'][1:], np.array([int(meditation)]))
             data['delta'] = np.append(data['delta'][1:], np.array([masaki[0]]))
             data['theta'] = np.append(data['theta'][1:], np.array([masaki[1]]))
-            data['lowalpha'] = np.append(data['lowalpha'][1:], np.array([masaki[2]]))
-            data['highalpha'] = np.append(data['highalpha'][1:], np.array([masaki[3]]))
-            data['lowbeta'] = np.append(data['lowbeta'][1:], np.array([masaki[4]]))
-            data['highbeta'] = np.append(data['highbeta'][1:], np.array([masaki[5]]))
-            data['lowgamma'] = np.append(data['lowgamma'][1:], np.array([masaki[6]]))
-            data['midgamma'] = np.append(data['midgamma'][1:], np.array([masaki[7]]))
-            data['highalpha_'] = np.append(data['highalpha_'][1:], np.array([int(highalpha)]))
-            data['lowgamma_'] = np.append(data['lowgamma_'][1:], np.array([int(lowgamma)]))
+            data['lowalpha'] = np.append(
+                data['lowalpha'][1:], np.array([masaki[2]]))
+            data['highalpha'] = np.append(
+                data['highalpha'][1:], np.array([masaki[3]]))
+            data['lowbeta'] = np.append(
+                data['lowbeta'][1:], np.array([masaki[4]]))
+            data['highbeta'] = np.append(
+                data['highbeta'][1:], np.array([masaki[5]]))
+            data['lowgamma'] = np.append(
+                data['lowgamma'][1:], np.array([masaki[6]]))
+            data['midgamma'] = np.append(
+                data['midgamma'][1:], np.array([masaki[7]]))
+            data['highalpha_'] = np.append(
+                data['highalpha_'][1:], np.array([int(highalpha)]))
+            data['lowgamma_'] = np.append(
+                data['lowgamma_'][1:], np.array([int(lowgamma)]))
             RP.set_data(data)
             RP.pause(0.1)
