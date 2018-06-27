f7993caa23411e0d0c5bf49be0af2518cdfec4e4
Masaki Aota <masaki.aota@ruri.waseda.jp>
2018-06-27 13:35:48 +0900

    socket通信を用いて、python2から3のプログラムに通信することに成功した

--- graph2.py
+++ graph2.py
@@ -13,50 +13,54 @@ PORT = '/dev/tty.MindWaveMobile-SerialPo'
 font = {'family': 'monospace', 'size': '9'}
 mpl.rc('font', **font)
 
+
 class realtime_plot(object):
     """
     リアルタイムでグラフ描画するクラス
     メソッドにはinitialize, set_data, pauseが入ってる
     呼び出されたら__init__でデータ数をself.numberに入れ，initializeを呼び出す
     """
+
     def __init__(self):
         # 最初にmasakiを渡してデータがいくつあるかをself.numberに入れる initializeを呼び出す
         self.number = 11
         self.fig = plt.figure(figsize=(12, 8))
         self.initialize()
-        
+
     def position(self):
-        ## 各プロットの位置を決める
+        # 各プロットの位置を決める
         # plt.subplot2gridで3x4で分割するとしている
         self.pos = {}
         for i in range(self.number):
-            self.pos[i,0] = i / 4 
-            self.pos[i,1] = (i - self.pos[i,0]) % 4
+            self.pos[i, 0] = i / 4
+            self.pos[i, 1] = (i - self.pos[i, 0]) % 4
 
     def initialize(self):
-        ## 最初のプロットの設定
+        # 最初のプロットの設定
         self.fig.suptitle('monitoring sample', size=12)
-        self.fig.subplots_adjust( left=0.05, bottom=0.1, right=0.95, top=0.90, wspace=0.2, hspace=0.6)
+        self.fig.subplots_adjust(
+            left=0.05, bottom=0.1, right=0.95, top=0.90, wspace=0.2, hspace=0.6)
         self.position()
         # self.plotに軸やタイトルなどの設定をする
         # self.linesにプロットする線の情報を入れる
         self.plot = {}
         self.lines = {}
         for i in range(self.number):
-            self.plot[i] = plt.subplot2grid((3,4), (self.pos[i,0], self.pos[i,1]))
+            self.plot[i] = plt.subplot2grid(
+                (3, 4), (self.pos[i, 0], self.pos[i, 1]))
             self.plot[i].grid(True)
-            self.plot[i].set_title(str(i)) # ここにmasakiでの名前入れたい
+            self.plot[i].set_title(str(i))  # ここにmasakiでの名前入れたい
             self.plot[i].set_xlabel('x')
             self.plot[i].set_ylabel('y')
             # プロットの初期化
             self.lines[i], = self.plot[i].plot([-1, -1], [1, 1], label=str(i))
 
     def set_data(self, data):
-    # 値名と値を代入した辞書タイプのdataから，必要なデータをsubplotの値に代入します
+        # 値名と値を代入した辞書タイプのdataから，必要なデータをsubplotの値に代入します
         for i in range(self.number):
             self.lines[i].set_data(data[0], data[i+1])
             self.plot[i].set_xlim((data[0].min(), data[0].max()))
-            self.plot[i].set_ylim((-5,5))
+            self.plot[i].set_ylim((-5, 5))
 #        import pdb; pdb.set_trace()
 
     def pause(self, second):
@@ -66,7 +70,7 @@ class realtime_plot(object):
 # xの初期化
 x = np.arange(-5, 5, 0.1)
 
-#plot用のデータを格納するdata
+# plot用のデータを格納するdata
 data = {}
 data[0] = x
 for i in range(11):
@@ -80,9 +84,9 @@ print(calib[1])
 
 while(True):
     try:
-        #ここでmasaki更新する
-#        for i in range(n):
-#            masaki[i] = time + i
+        # ここでmasaki更新する
+        #        for i in range(n):
+        #            masaki[i] = time + i
         count = 1
         attention, meditation = 0, 0
         df = []
@@ -106,9 +110,11 @@ while(True):
                         masaki = myfunc.get_nouha(eeg)
                         masaki_t = myfunc.transform(masaki, calib[0], calib[1])
                         # data[0]にx軸のデータを格納
-                        data[0] = np.arange(-5 + 0.1 * count, 5 + 0.1 * count, 0.1)
+                        data[0] = np.arange(-5 + 0.1 * count,
+                                            5 + 0.1 * count, 0.1)
                         for i in range(11):
-                            data[i+1] = np.append(data[i+1][1:], np.array([masaki_t[i]]))
+                            data[i+1] = np.append(data[i+1]
+                                                  [1:], np.array([masaki_t[i]]))
 #                                    data[i+1] = np.append(data[i+1][1:], count)
                         RP.set_data(data)
                         count = count + 1
---
+++ socket_send.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+https://qiita.com/nadechin/items/28fc8970d93dbf16e81b
+'''
+import socket
+import time
+
+host = "127.0.0.1"  # ローカルホストを指定
+port = 50007  # 適当なPORTを指定してあげます
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # オブジェクトの作成をします
+client.connect((host, port))  # これでサーバーに接続します
+
+
+if __name__ == '__main__':
+    test = 1
+    while (1):
+        print 'sent', test
+        client.send(str(test))
+        test = test + 1
+        time.sleep(0.5)
